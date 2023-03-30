938d20bd9bd38b0bb479171ffe7ba56458291ee5
LeLinux <lelinuxgit@gmail.com>
2023-03-30 14:38:31 +0300

    vector alternative completed

--- vector_main.py
+++ vector_main.py
@@ -174,12 +174,12 @@ for i in range(1152):
 layers.append(tmp_mass)
 
 tmp_mass = []
-for i in range(600):
+for i in range(500):
 	tmp_mass.append(0)
 layers.append(tmp_mass)
 
 tmp_mass = []
-for i in range(400):
+for i in range(350):
 	tmp_mass.append(0)
 layers.append(tmp_mass)
 
@@ -304,7 +304,7 @@ for i in range(100):
 	print("time", time.time()- s_t)
 	#print(layers[2])
 	#print(create_output_pixels("./x_train_4824/IMG_1089.BMP"))
-	#back_evolve(create_output_pixels("./test_output.BMP"))
+	back_evolve(create_output_pixels("./test_output.BMP"))
 	print(weights[0][1][0])
 	if(i % 10 == 0):
 		pass#print(layers[3])
@@ -318,20 +318,85 @@ for i in range(100):
 					string += str("0")
 			print(string)'''
 
-layers[0] = create_input_pixels("./changes_origin_images_4824/IMG_1089.JPG")
+layers[0] = create_input_pixels("./test_input.JPG")
 layer_evolve(layers[0], layers[1], weights[0])
 layer_evolve(layers[1], layers[2], weights[1])
 layer_evolve(layers[2], layers[3], weights[2])
 
+matrix = {"0" :  [0, 0, 0, 0],
+          '1' :  [1, 0, 0, 0],
+          '2' :  [0, 1, 0, 0],
+          '3' :  [0, 0, 1, 0],
+          '4' :  [0, 0, 0, 1],
+          '5' :  [1, 1, 0, 0],
+          '6' :  [1, 0, 1, 0],
+          '7' :  [1, 0, 0, 1],
+          '8' :  [0, 1, 1, 0],
+          '9':  [0, 1, 0, 1],
+          'a':  [0, 0, 1, 1],
+          'b':  [1, 1, 1, 0],
+          'c':  [1, 1, 0, 1],
+          'd':  [1, 0, 1, 1],
+          'e':  [0, 1, 1, 1],
+          'f':  [1, 1, 1, 1]}
+
 print(layers[3])
 for i in range(24):
 	string = ""
-	for j in range(48):
-		#print(mass[32 * i + j])
-		if float(layers[3][48 * i + j]) >= 128:
-			string += str("1")
-		else:
-			string += str("0")
+	for j in range(12):
+		x = layers[3][12 * i + j]
+		if(x <= 50):
+			data = matrix["0"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 50 and x <= 150):
+			data = matrix["1"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 150 and x <= 250):
+			data = matrix["2"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 250 and x <= 350):
+			data = matrix["3"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+
+		elif(x > 350 and x <= 450):
+			data = matrix["4"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 450 and x <= 550):
+			data = matrix["5"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 550 and x <= 650):
+			data = matrix["6"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 650 and x <= 750):
+			data = matrix["7"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+
+		elif(x > 750 and x <= 850):
+			data = matrix["8"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 850 and x <= 950):
+			data = matrix["9"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 950 and x <= 1050):
+			data = matrix["a"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 1050 and x <= 1150):
+			data = matrix["b"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+
+		elif(x > 1150 and x <= 1250):
+			data = matrix["c"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 1250 and x <= 1350):
+			data = matrix["d"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 1350 and x <= 1450):
+			data = matrix["e"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+		elif(x > 1450):
+			data = matrix["f"]
+			string += str(data[0]) + str(data[1]) + str(data[2])+ str(data[3])
+
 	print(string)
 
 print("\n")
